f9ea3d7724c76018cfc4991c89fa0f1de0063b70
Serge du Preez <sergedupreez@gmail.com>
2019-04-17 17:08:35 -0600

    Clark Kent Assignment

---
+++ exercises/grocery shopping/shopping.js
@@ -0,0 +1,9 @@
+var shopper = {
+    isBargaining: true,
+    age = 26,
+    firstName = 'John',
+
+    // groceryCart: ['eggs , milk, bread']
+}
+
+shopper.grocerCart = []
---
+++ exercises/workflow/workflowConventions.js
@@ -0,0 +1,26 @@
+// //camelCase
+// var myVariableIsNamedVar = 'this is my variable';
+
+// //white space
+
+// var myVar = 'stuff'
+// if(myVar==='things')
+//     {console.log('cool')}
+// else
+//     {console.log('dumb');
+// }
+
+// //conventions
+
+// var prettySum = 4 * 3 / 2 + (5 - 1
+// );
+
+// var person = {
+//     name='bob',
+//     age=32
+// }
+
+// var isAlive = true
+
+// var numbers = [1,2,3,4,5,6]
+// var animals = ['dog', 'cat','bird']
